9af513af6c1e5a706bd29f8eaa33715e02c0e17d
rykn0wxx <raurici666@gmail.com>
2018-08-12 05:48:49 +0800

    done with button and input

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,22 @@
 'use strict';
-(function(window, $) {
-	console.log($, 'tuuoio');
+// (function(window, $) {
+// 	console.log($, 'tuuoio');
 	
-})(window, jQuery);
+// })(window, jQuery);
+
+(function () {
+
+	function _run (msg) {
+		$('.input-container .md-input').on('focus blur', function (i) {
+			$(this).parents('.form-group').toggleClass('md-input-focused', 'focus' === i.type || this.value.length > 0);
+		});
+		console.log(msg);
+	}
+
+	if (document.readyState !== 'loading') _run('readyState loading');
+	else if (document.addEventListener) document.addEventListener('DOMContentLoaded', _run('domContentLoaded'));
+	else document.attachEvent('onreadystatechange', function(){
+		if (document.readyState=='complete') _run('stateChange complete');
+	});
+
+}).call(this);
